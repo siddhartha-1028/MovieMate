c490ff3acf958de37f48e75c2153b7207ff2cd5d
Siddhartha Pasumarthy <siddharthapasumarthy07@gmail.com>
2025-07-04 10:38:42 +0530

    Implementation of User Authentication

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
-from flask import Flask, render_template, request
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 
+load_dotenv()
+
 app= Flask(__name__)
-API_KEY= 'ab3fc89f40fa5a5313c8c7599f07ca1b'
+app.secret_key = os.getenv('SECRET_KEY')
+API_KEY = os.getenv('API_KEY')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/home')
+def home():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    return render_template('home.html')
 
 @app.route('/search')
 def search():
@@ -16,6 +38,48 @@ def search():
     data= response.json()
     movies= data['results']
     return render_template('results.html',movies=movies)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists. Try another."
+
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+        else:
+            return "Invalid credentials."
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
 
 if __name__ =='__main__':
     app.run(debug=True)
